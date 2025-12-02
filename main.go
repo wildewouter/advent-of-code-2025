7b04a9a002dc9b0530ce38a363c388cf9807c07d
Wouter de Wild <wouter@codewild.nl>
2025-12-02 08:56:16 +0100

    Added timing

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
@@ -19,14 +20,20 @@ func main() {
 
 	switch *day {
 	case 1:
+		start := time.Now()
 		one, err := day01.PartOne(string(data))
+		partOne := time.Since(start)
+		start = time.Now()
 		two, err := day01.PartTwo(string(data))
+		partTwo := time.Since(start)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		fmt.Println("Part One: ", one)
+		fmt.Println("Time (Part One): ", partOne)
 		fmt.Println("Part Two: ", two)
+		fmt.Println("Time (Part Two): ", partTwo)
 	default:
 		log.Fatalf("day %d not implemented", *day)
 	}
