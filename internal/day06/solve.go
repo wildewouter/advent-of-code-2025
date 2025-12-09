23ea25bf5d92bb4898320466c176a8a85f022524
Wouter de Wild <wouter@codewild.nl>
2025-12-09 20:21:28 +0100

    Day 6 part two

--- internal/day06/solve.go
+++ internal/day06/solve.go
@@ -6,10 +6,84 @@ import (
 	"strings"
 )
 
-func PartOne(input string) (int, error) {
-	lines := strings.Split(input, "\n")
+var re = regexp.MustCompile(`\s+`)
 
-	re := regexp.MustCompile(`\s+`)
+func PartOne(input string) (int, error) {
+	lines, operations := getOperations(input)
+
+	for _, line := range lines {
+		stringValues := strings.Split(strings.TrimSpace(re.ReplaceAllString(line, " ")), " ")
+		for i, value := range stringValues {
+			v, _ := strconv.Atoi(value)
+			operations[i].values = append(operations[i].values, v)
+		}
+	}
+
+	answer := calc(operations)
+
+	return answer, nil
+}
+
+func PartTwo(input string) (int, error) {
+	lines, operations := getOperations(input)
+
+	runeGrid := map[Coord]rune{}
+
+	for y, line := range lines {
+		runes := []rune(line)
+
+		for x, r := range runes {
+			runeGrid[Coord{x: x, y: y}] = r
+		}
+	}
+
+	var groups [][]int
+
+	series := 0
+
+	for x := 0; x <= len(lines[0]); x++ {
+		values := ""
+		for y := 0; y < len(lines); y++ {
+			r := runeGrid[Coord{x, y}]
+
+			if r == ' ' {
+				continue
+			}
+
+			values += string(r)
+		}
+
+		if values == "" {
+			series++
+			continue
+		}
+
+		if len(groups) <= series {
+			groups = append(groups, []int{})
+		}
+
+		atoi, _ := strconv.Atoi(values)
+		groups[series] = append(groups[series], atoi)
+	}
+
+	for i, _ := range operations {
+		for _, v := range groups[i] {
+			operations[i].values = append(operations[i].values, v)
+		}
+	}
+
+	answer := calc(operations)
+
+	return answer, nil
+}
+
+type Coord struct {
+	x int
+	y int
+}
+
+func getOperations(input string) ([]string, []Operation) {
+	lines := strings.Split(input, "\n")
 
 	operatorLine := lines[len(lines)-1]
 	operators := strings.Split(strings.TrimSpace(re.ReplaceAllString(operatorLine, " ")), " ")
@@ -22,15 +96,10 @@ func PartOne(input string) (int, error) {
 		operations = append(operations, Operation{operator, []int{}})
 	}
 	operators = nil
+	return lines, operations
+}
 
-	for _, line := range lines {
-		stringValues := strings.Split(strings.TrimSpace(re.ReplaceAllString(line, " ")), " ")
-		for i, value := range stringValues {
-			v, _ := strconv.Atoi(value)
-			operations[i].values = append(operations[i].values, v)
-		}
-	}
-
+func calc(operations []Operation) int {
 	answer := 0
 
 	for _, operation := range operations {
@@ -48,11 +117,7 @@ func PartOne(input string) (int, error) {
 		answer += opAns
 	}
 
-	return answer, nil
-}
-
-func PartTwo(input string) (int, error) {
-	return 0, nil
+	return answer
 }
 
 type Operation struct {
